fa25e63a3c6727aa25da63ea0f6c051814412953
swestfechtel <33691411+swestfechtel@users.noreply.github.com>
2019-05-11 16:00:55 +0200

    gradle fix

--- app/src/androidTest/java/com/fhaachen/ip_ritz/prototyp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/fhaachen/ip_ritz/prototyp/ExampleInstrumentedTest.java
@@ -1,8 +1,9 @@
 package com.fhaachen.ip_ritz.prototyp;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
