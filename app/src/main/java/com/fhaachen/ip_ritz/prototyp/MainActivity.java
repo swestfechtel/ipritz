698d759afda7d3e562c51109e6e19498cb70cab9
lena <lenasauermann99@gmail.com>
2019-06-12 18:57:08 +0200

    current locations works again ( i dont know why)

--- app/src/main/java/com/fhaachen/ip_ritz/prototyp/MainActivity.java
+++ app/src/main/java/com/fhaachen/ip_ritz/prototyp/MainActivity.java
@@ -327,7 +327,6 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-
     @Override
     public void onConnected(Bundle bundle) {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
